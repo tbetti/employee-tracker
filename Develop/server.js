dbef9d9063c84b344a57d778f14a307adaa4a6d1
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-27 14:10:56 -0400

    Moved option function to util.js to help with organization

--- Develop/server.js
+++ Develop/server.js
@@ -1,5 +1,6 @@
 const inquirer = require('inquirer');
 const questions = require('./util/inquire_questions.js');
+const option = require('./util/util.js');
 const sequelize = require('./config/connection.js');
 const Department = require('./models/department.js');
 const express = require('express');
@@ -16,45 +17,6 @@ function init() {
     .then((data) =>{
         option(data)
     })
-}
-
-// Display data or ask additional questions based on option selected
-function option(data) {
-    switch(data.options){
-        case 'View all departments':
-            console.log("Now viewing all departments");
-            break;
-        case 'View all roles':
-            console.log('Now viewing all roles');
-            break;
-        case 'View all employees':
-            console.log('Now viewing all employees');
-            break;
-        case 'Add a department':
-            inquirer.prompt(questions.addDepartment)
-            .then((data)=>{
-                console.log(data);
-            })
-            break;
-        case 'Add a role':
-            inquirer.prompt(questions.addRole)
-            .then((data) =>{
-                console.log(data);
-            })
-            break;
-        case 'Add an employee':
-            inquirer.prompt(questions.addEmployee)
-            .then((data) =>{
-                console.log(data);
-            })
-            break;
-        case 'Update an employee role':
-            inquirer.prompt(questions.updateEmployeeRole)
-            .then((data) =>{
-                console.log(data);
-            })
-            break;
-    }
 }
 
 // Connect to sequelize and listen on port 3001
