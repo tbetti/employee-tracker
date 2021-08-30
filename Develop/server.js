400131f4420bc3bda31129e9de3838673409d0c0
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-30 14:52:59 -0400

    Successfully prompted to add new role based on user input

--- Develop/server.js
+++ Develop/server.js
@@ -1,35 +1,35 @@
 const inquirer = require('inquirer');
 const questions = require('./util/inquire_questions.js');
-const {printTable} = require('console-table-printer');
+const { printTable } = require('console-table-printer');
 const DB = require('./util/util');
 
 // Prompt user for action
 function init() {
     inquirer.prompt(questions.options)
-    .then((data) =>{
-        option(data)
-    })
+        .then((data) => {
+            option(data)
+        })
 }
 
 // Display data or ask additional questions based on option selected
 function option(data) {
-    switch(data.options){
+    switch (data.options) {
         case 'View all departments':
-            DB.viewDepartments().then(res=>{
+            DB.viewDepartments().then(res => {
                 console.log('\n\n')
                 printTable(res);
                 init();
             })
             break;
         case 'View all roles':
-            DB.viewRoles().then(res =>{
+            DB.viewRoles().then(res => {
                 console.log('\n\n')
                 printTable(res);
                 init();
             })
             break;
         case 'View all employees':
-            DB.viewEmployees().then(res=>{
+            DB.viewEmployees().then(res => {
                 console.log('\n\n')
                 printTable(res);
                 init();
@@ -37,47 +37,43 @@ function option(data) {
             break;
         case 'Add a department':
             inquirer.prompt(questions.addDepartment)
-            .then((data)=>{
-                DB.addDepartment({department_name: data.department_name}).then(res=>{
-                    if(res.affectedRows === 1){
-                        console.log(`Successfully added ${data.department_name}`);
-                    }
-                    init();
-                });
-            })
+                .then((data) => {
+                    DB.addDepartment({ department_name: data.department_name }).then(res => {
+                        if (res.affectedRows === 1) {
+                            console.log(`Successfully added ${data.department_name}`);
+                        }
+                        init();
+                    });
+                })
             break;
         case 'Add a role':
             inquirer.prompt(questions.addRole)
-            .then((data) =>{
-                DB.findId(data.department, 'departments', 'department_name').then(res=>{
-                    result = JSON.stringify(res);
-                    console.log(res[0].id);
+                .then((data) => {
+                    DB.findId(data.department, 'departments', 'department_name').then(res => {
+                        DB.addRole({
+                            title: data.newRole,
+                            salary: data.salary,
+                            department_id: res[0].id
+                        }).then(res => {
+                            if (res.affectedRows === 1) {
+                                console.log(`Successfully added ${data.newRole} to ${data.department}`);
+                            }
+                            init();
+                        });
+                    })
                 })
-            })
-            // .then((data) =>{
-            //     DB.addRole({
-            //         title: data.newRole,
-            //         salary: data.salary,
-            //         department_id: findId(data.department, 'departments', 'department_name')
-            //     }).then(res=>{
-            //         if(res.affectedRows === 1){
-            //             console.log(`Successfully added ${data.newRole} to ${data.department}`);
-            //         }
-            //         init();
-            //     });
-            // }) 
             break;
         case 'Add an employee':
             inquirer.prompt(questions.addEmployee)
-            .then((data) =>{
-                console.log(data);
-            })
+                .then((data) => {
+                    console.log(data);
+                })
             break;
         case 'Update an employee role':
             inquirer.prompt(questions.updateEmployeeRole)
-            .then((data) =>{
-                console.log(data);
-            })
+                .then((data) => {
+                    console.log(data);
+                })
             break;
     }
 }
