d5283330b52d74805917cb01ace270abe87fefc9
Tiana Bettinson <tiana.bettinson@gmail.com>
2021-08-30 09:13:16 -0400

    Added values to the departments, roles, and employees tables

--- Develop/db/seeds.sql
+++ Develop/db/seeds.sql
@@ -1,9 +1,29 @@
-INSEERT INTO departments (name)
-VALUES (
-    "IT",
-    "Finance",
-    "Advertising",
-    "Sales"
-);
+INSERT INTO departments (department_name)
+VALUES 
+    ("IT"),
+    ("Finance"),
+    ("Advertising"),
+    ("Sales")
+;
 
-INSERT INTO 
+INSERT INTO roles (id, title, salary)
+VALUES 
+    (1, "Relations Manager", "70000"),
+    (2, "Secretary", "40000"),
+    (3, "Designer", "75000"),
+    (4, "Salesperson", "60000")
+;
+
+UPDATE roles
+SET id=LPAD(FLOOR(RAND() * 999999.99), 6, '0');
+
+INSERT INTO employees (id, first_name, last_name, manager_id)
+VALUES 
+    (1, "Jen", "Barber", 1),
+    (2, "Charlie", "Smith", NULL),
+    (3, "Izzy", "Yazzie", NULL),
+    (4, "Nelly", "Jones", NULL)
+;
+
+UPDATE employees
+SET id=LPAD(FLOOR(RAND() * 999999.99), 6, '0');
